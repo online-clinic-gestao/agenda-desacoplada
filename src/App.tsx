e74b2bee687d00d047ef8e22b064d021c351772d
Alex Morte <alex@onlineclinic.com.br>
2025-04-10 22:10:32 -0300

    Project Started

---
+++ src/App.tsx
@@ -0,0 +1,209 @@
+import React, { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { DoctorCard } from './components/DoctorCard/DoctorCard'
+import Typography from '@mui/material/Typography'
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import Box from '@mui/material/Box';
+import StepButton from '@mui/material/StepButton';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+const steps = ['Dados Pessoais', 'Agendamento', 'Conclusão'];
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [completed, setCompleted] = React.useState<{
+    [k: number]: boolean;
+  }>({});
+
+  const totalSteps = () => {
+    return steps.length;
+  };
+
+  const completedSteps = () => {
+    return Object.keys(completed).length;
+  };
+
+  const isLastStep = () => {
+    return activeStep === totalSteps() - 1;
+  };
+
+  const allStepsCompleted = () => {
+    return completedSteps() === totalSteps();
+  };
+
+  const handleNext = () => {
+    const newActiveStep =
+      isLastStep() && !allStepsCompleted()
+        ? // It's the last step, but not all steps have been completed,
+          // find the first step that has been completed
+          steps.findIndex((step, i) => !(i in completed))
+        : activeStep + 1;
+    setActiveStep(newActiveStep);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStep = (step: number) => () => {
+    setActiveStep(step);
+  };
+
+  const handleComplete = () => {
+    setCompleted({
+      ...completed,
+      [activeStep]: true,
+    });
+    handleNext();
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+    setCompleted({});
+  };
+
+  const PersonalData = () => {
+      return <Box>
+        <FormControl fullWidth sx={{ m: 1 }}>
+            <TextField id="outlined-basic" label="Nome completo" variant="outlined" />
+        </FormControl>
+        <FormControl fullWidth sx={{ m: 1 }}>
+            <TextField id="outlined-basic" label="Telefone" variant="outlined" />
+        </FormControl>
+        <FormControl fullWidth sx={{ m: 1 }}>
+            <TextField label="Email" variant="outlined" />
+        </FormControl>
+      </Box>
+  }
+
+  const ExamData = () => {
+        return <> <FormControl fullWidth sx={{m:1}}>
+          <InputLabel id="demo-simple-select-label">Procedimento</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={12}
+            label="Procedimento"
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl fullWidth sx={{m:1}}>
+          <InputLabel id="demo-simple-select-label">Convênio</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={12}
+            label="Convênio"
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl></>
+  }
+
+
+
+  return (
+    <>
+        <Typography>
+            Onlineclinic
+        </Typography>
+
+        <hr />
+        <Box sx={{ width: '100%' }}>
+      <Stepper nonLinear activeStep={activeStep}>
+        {steps.map((label, index) => (
+          <Step key={label} completed={completed[index]}>
+            <StepButton color="inherit" onClick={handleStep(index)}>
+              {label}
+            </StepButton>
+          </Step>
+        ))}
+      </Stepper>
+      <div>
+        {allStepsCompleted() ? (
+          <React.Fragment>
+            <Typography sx={{ mt: 2, mb: 1 }}>
+              All steps completed - you&apos;re finished
+            </Typography>
+            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+              <Box sx={{ flex: '1 1 auto' }} />
+              <Button onClick={handleReset}>Reset</Button>
+            </Box>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <Typography sx={{ mt: 2, mb: 1, py: 1 }}>
+                {activeStep === 0 ? <PersonalData /> : null}
+                {activeStep === 1 ? <ExamData /> : null}
+            </Typography>
+            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+              <Button
+                color="inherit"
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                sx={{ mr: 1 }}
+              >
+                Back
+              </Button>
+              <Box sx={{ flex: '1 1 auto' }} />
+              <Button onClick={handleNext} sx={{ mr: 1 }}>
+                Next
+              </Button>
+              {activeStep !== steps.length &&
+                (completed[activeStep] ? (
+                  <Typography variant="caption" sx={{ display: 'inline-block' }}>
+                    Step {activeStep + 1} already completed
+                  </Typography>
+                ) : (
+                  <Button onClick={handleComplete}>
+                    {completedSteps() === totalSteps() - 1
+                      ? 'Finish'
+                      : 'Complete Step'}
+                  </Button>
+                ))}
+            </Box>
+          </React.Fragment>
+        )}
+      </div>
+    </Box>
+        <DoctorCard />
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/DoctorCard/DoctorCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const DoctorCard = () => {
+    return (
+        <div className="doctor-card">
+        <h2>Doctor's Name</h2>
+        <p>Specialization</p>
+        <p>Experience: X years</p>
+        <button>Book Appointment</button>
+        </div>
+    ); 
+}
